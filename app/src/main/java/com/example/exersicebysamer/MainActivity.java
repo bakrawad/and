5c98d74e127af1e70fb57beb31df7c357404a9cc
bakr <bakr0592260950@gmail.com>
2024-04-03 13:31:13 +0200

    .

--- app/src/main/java/com/example/exersicebysamer/MainActivity.java
+++ app/src/main/java/com/example/exersicebysamer/MainActivity.java
@@ -19,7 +19,7 @@ import androidx.core.view.WindowInsetsCompat;
 public class MainActivity extends AppCompatActivity {
 
     EditText email;
-    EditText name;
+    EditText name;//sda
 
     RadioGroup group;
     RadioButton male;
